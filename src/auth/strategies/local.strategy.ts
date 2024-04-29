0d796c4e05b43272958b4e9eeb11496bb5b551ce
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-04-29 07:53:54 +0100

    fix deployment bug

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Strategy } from 'passport-local';
 import { UsersService } from 'src/users/users.service';
 
@@ -10,6 +11,11 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
       usernameField: 'email',
     });
   }
+  @ApiOperation({ summary: 'Log in' })
+  @ApiResponse({
+    status: 200,
+    description: 'Log in a user',
+  })
   public async validate(email: string, password: string) {
     console.log(email, password);
     const user = await this.usersService.verifyEmailAndPassword(
